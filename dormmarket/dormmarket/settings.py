436e4ac050bc05bee6faad03f94dfb038fc651cf
m-99 <sammiller1999@hotmail.com>
2018-09-15 15:11:14 -0400

    added main to installed apps, added basic views

--- dormmarket/dormmarket/settings.py
+++ dormmarket/dormmarket/settings.py
@@ -28,7 +28,6 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -38,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'main',
 ]
 
 MIDDLEWARE = [
--- dormmarket/main/views.py
+++ dormmarket/main/views.py
@@ -1,5 +1,22 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+# first thing that user sees -> browse
 def index(request):
     return HttpResponse("hey bb ur at the main index")
+
+# show an item in detail
+def focused(request):
+    return HttpResponse("hey bb ur at focused")
+
+# a form to submit a buy request for a type of item
+def submit_buy(request):
+    return HttpResponse("hey bb ur at submit_buy")
+
+# a form to submit an item to be sold in the market
+def submit_sell(request):
+    return HttpResponse("hey bb ur at submit_sell")
+
+# view items that you are selling in the marketplace
+def sell_list(request):
+    return HttpResponse("hey bb ur at sell_list")
